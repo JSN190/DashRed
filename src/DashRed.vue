db91db1fb6011c3025238eb47df69168450de348
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-19 03:33:24 +0100

    Animation improvements:

--- src/DashRed.vue
+++ src/DashRed.vue
@@ -18,20 +18,20 @@ along with this program.  If not, see https://www.gnu.org/licenses.
     <div id="app" class="wrapper">
         <main class="main-container">
             <div class="main-group-1">
-                <div class="main-logo-tag-group zoomIn animated faster">
+                <div class="main-logo-tag-group zoomIn animated fast">
                     <div class="main-logo">Dash<span class="reddit-orange">Red</span></div>
                     <div class="main-tag-line">
                         Get direct, proper shareable links to your reddit videos.
                     </div>
                 </div>
-                <div id="main-input-container" class="bounceInUp faster animated">
+                <div id="main-input-container" class="bounceInUp fast animated">
                     <input id="main-input-url" v-model="inputUrl" placeholder="Type or paste your reddit link here..."
                         @keyup.enter="submit" autofocus>
                     <span id="main-input-submit" v-on:click="submit"><i class="fas fa-arrow-right"></i></span>
                 </div>
             </div>
         </main>
-        <footer class="bounceInUp animated">
+        <footer class="bounceInUp slow animated">
             <div>
                 Released under <a href="https://github.com/JSN190/DashRed"><i class="fab fa-osi"></i> GNU AGPL v3.0</a>. 
                 Created by <a href="https://github.com/JSN190"><i class="fab fa-github"></i> JSN190</a>.
@@ -68,6 +68,7 @@ export default {
             this.animatePreSubmit();
             const regexRedditLink = /https:\/\/(www|old).reddit.com\/r\/[a-z]+\/comments\/[a-zA-Z0-9]+\/[a-zA-Z0-9_-]+/;
             const matched = this.inputUrl.trim().match(regexRedditLink) ? this.inputUrl.match(regexRedditLink)[0] : null;
+            console.log(`${matched}.json`)
             fetch(`${matched}.json`)
             .then(response => response.json()).then(data =>
                 this.inputUrl = data[0].data.children[0].data.media.reddit_video.fallback_url)
